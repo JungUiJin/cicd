233bf2a179591a25fc992b0b14c28f88ed9e4a65
JungUiJin <yealkki38@gmail.com>
2024-11-10 23:50:25 +0900

    axios 업데이트

--- MENDITTZO-Frontend/src/main.js
+++ MENDITTZO-Frontend/src/main.js
@@ -4,7 +4,7 @@ import router from "@/router/router.js";
 import axios from "axios";
 import {createPinia} from "pinia";
 
-axios.defaults.baseURL = '/boot'; // ingress 설정으로 인해 변경
+axios.defaults.baseURL = '/boot/api/v1'; // ingress 설정으로 인해 변경
 const pinia = createPinia();
 
 const app = createApp(App); // Vue 애플리케이션 생성
